cbfcd21d23a4b54380947161691f323e60079dbb
HugoOsornio <victor.hugo.osornio@gmail.com>
2021-06-30 23:24:23 -0700

    Parity Code

--- epi_judge_cpp/parity.cc
+++ epi_judge_cpp/parity.cc
@@ -1,7 +1,34 @@
 #include "test_framework/generic_test.h"
+/*short Parity(unsigned long long x) {
+    int parity = 0;
+    while (x)
+    {
+        parity ^= (x & 1);
+        x = x >> 1;
+    }
+  return parity;
+}*/
+
+/*
 short Parity(unsigned long long x) {
-  // TODO - you fill in here.
-  return 0;
+    short result = 0;
+    while (x)
+    {
+        result ^= 1;
+        x &= (x - 1);
+    }
+    return result%2;
+}
+*/
+
+short Parity(unsigned long long x) {
+    x ^= x >> 32;
+    x ^= x >> 16;
+    x ^= x >> 8;
+    x ^= x >> 4;
+    x ^= x >> 2;
+    x ^= x >> 1;
+    return x & 1;
 }
 
 int main(int argc, char* argv[]) {
