9144841a2dcf5c20b41be8990c0a2fdb52e0a56b
HugoOsornio <victor.hugo.osornio@gmail.com>
2021-07-02 15:46:50 -0700

    The reverse problem can be attacked via 2 ways. 1: Swapping the bits one by one: IE Swap bit 0 with 63, swap bit 1 with 62 and so on.
    2: Using a cached approach, create a cache of 2^16 elements, where each
    element index, contains the reverse of its index, then divide the 64 bit
    in 4 chunks (of 16 bits each), reverse each one of those chunks, and
    then store it in a final 64bits by shifting the reversed chunks as
    needed.

---
+++ epi_judge_cpp/precomputedReverses.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <array>
+
+extern std::array<unsigned long long, 1 << 16> precomputedReverse;
--- epi_judge_cpp/reverse_bits.cc
+++ epi_judge_cpp/reverse_bits.cc
@@ -1,8 +1,55 @@
 #include "test_framework/generic_test.h"
-unsigned long long ReverseBits(unsigned long long x) {
-  // TODO - you fill in here.
-  return 0;
+#include "precomputedReverses.h"
+
+unsigned long long swap(unsigned long long x, uint32_t i, uint32_t j)
+{
+    int bitAtI = (x >> i) & 1;
+    int bitAtJ = (x >> j) & 1;
+
+    if (bitAtI != bitAtJ)
+    {
+        x ^= (unsigned long long)1 << i;
+        x ^= (unsigned long long)1 << j;
+    }
+    return x;
 }
+
+/*
+unsigned long long ReverseBits(unsigned long long x) 
+{
+    for (uint32_t i = 0; i < 32; i++)
+    {
+        x = swap(x, i, 63 - i);
+    }
+    return x;
+}
+*/
+
+unsigned long long ReverseBits(unsigned long long x)
+{
+    uint32_t cacheBits = 16;
+    uint32_t cacheMask = 0xFFFF;
+
+    unsigned long long firstQuarter = (x >> 3 * cacheBits) & cacheMask;
+    firstQuarter = precomputedReverse[firstQuarter];
+
+    unsigned long long secondQuarter = (x >> 2 * cacheBits) & cacheMask;
+    secondQuarter = precomputedReverse[secondQuarter];
+    secondQuarter <<= cacheBits;
+
+    unsigned long long thirdQuarter = (x >> cacheBits) & cacheMask;
+    thirdQuarter = precomputedReverse[thirdQuarter];
+    thirdQuarter <<= cacheBits * 2;
+
+    unsigned long long fourthQuarter = x & cacheMask;
+    fourthQuarter = precomputedReverse[fourthQuarter];
+    fourthQuarter <<= cacheBits * 3;
+
+    return fourthQuarter | thirdQuarter | secondQuarter | firstQuarter;
+}
+
+// 10110100010001100100011010101011100100000000000000000000000
+// 10110100010001100100011010101011100100000000000000000000000
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
